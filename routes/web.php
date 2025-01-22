85f659c5dcd63657737a452269c34da88829a098
Arif27295 <mdarifhowlader091@gmail.com>
2025-01-22 21:14:20 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\ProfileController;
@@ -30,5 +29,5 @@ Route::post('/upzila', [adminController::class, 'upzila'])->name('upzila');
 Route::post('/post_office', [adminController::class, 'post_office'])->name('post_office');
 Route::post('/post_code', [adminController::class, 'post_code'])->name('post_code');
 
+Route::get('/student_list', [adminController::class, 'student_list'])->name('student_list');
 Route::post('/student', [adminController::class, 'create_student'])->name('create_student');
-
