a1888c91257d4daf6d27f61cc240b4ea8daea987
Arif27295 <mdarifhowlader091@gmail.com>
2025-01-24 19:47:03 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::post('/post_code', [adminController::class, 'post_code'])->name('post_cod
 
 Route::get('/student_list', [adminController::class, 'student_list'])->name('student_list');
 Route::post('/student', [adminController::class, 'create_student'])->name('create_student');
+Route::get('/update_student_id/{id}', [adminController::class, 'update_student_id'])->name('update_student_id');
+Route::get('/delete_student_id/{id}', [adminController::class, 'delete_student_id'])->name('delete_student_id');
+Route::put('/update_student/{id}', [adminController::class, 'update_student'])->name('update_student');
